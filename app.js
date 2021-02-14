63e95ea5406f5a708090f4eec37c5b50bdb7ea6e
Patryk <47945758+M-Patryk@users.noreply.github.com>
2021-02-14 18:54:10 +0100

    Added algo so it converts message to an encrypted base64 string

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 document.querySelector('form').addEventListener('submit', (event) => {
 	event.preventDefault(); //preventDefault dzialo tak, ze w momencie klikniecia przycisku
-    //przegladarka automatycznie sprobuje wyslac cos do backendu. to ja zatrzyma
-    
-    const input = document.querySelector('input')
-    console.log(input.value)
+	//przegladarka automatycznie sprobuje wyslac cos do backendu. to ja zatrzyma
+
+	const input = document.querySelector('#message-input');
+	const encrypted = btoa(input.value);
+
+	const linkInput = document.querySelector('#link-input');
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
 });
